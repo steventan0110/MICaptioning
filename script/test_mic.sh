feee24cd6fcb72e6b3f5fd3553110acad34309f3
Heidi Zhang <chenyu.heidi.z@gmail.com>
2021-04-26 17:34:53 -0400

    train&test scripts

---
+++ script/test_mic.sh
@@ -0,0 +1,12 @@
+WORK_DIR=/home/czhan105/MICaptioning
+TRAIN=$WORK_DIR/train.py
+DATA=/export/b02/wtan/MIC-dataset/datasets
+CHECKPOINT_FOLDER=/export/b02/czhan105/MIC-checkpoints
+CAPTION=/export/b02/wtan/MIC-dataset/iu_xray_captions.json
+
+python $TRAIN \
+    --mode test --cpu \
+    --caption-dir $CAPTION \
+    --data-dir $DATA \
+    --max-epoch 10 \
+    --load-dir $CHECKPOINT_FOLDER/checkpoint0.pt
