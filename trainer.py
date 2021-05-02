3025c5cb22fa247d11f217b9707723a3ffe49922
Jack Zhang <jzhan237@jhu.edu>
2021-05-02 21:49:12 +0800

    Add tag prediction using MLC

--- trainer.py
+++ trainer.py
@@ -1,14 +1,16 @@
 import torch
 import os
+import torch.nn.functional as F
 # train model for 1 epoch
 
 class Trainer():
-    def __init__(self, model, train_dataloader, valid_dataloader, **kwargs):
+    def __init__(self, model, train_dataloader, valid_dataloader, tl_scale=1, **kwargs):
         self.args = kwargs
         self.device = torch.device('cpu' if self.args['cpu'] else 'cuda')
         self.model = model.to(self.device)
         self.train_dataloader = train_dataloader
         self.valid_dataloader = valid_dataloader
+        self.tl_scale = tl_scale
         # TODO: constructing optimizer, scheduler can be modularized for different args passed in
         self.criterion = torch.nn.CrossEntropyLoss()
         self.optimizer = optimizer = torch.optim.Adam(self.model.parameters(), lr=self.args['learning_rate'])
@@ -38,16 +40,20 @@ class Trainer():
         val_steps = 0
         self.model.train()
         for i, (img, caption, tags_vec) in enumerate(self.train_dataloader):
-            img, caption = img.to(self.device), caption.to(self.device)
+            img, caption, tags_vec = img.to(self.device), caption.to(self.device), tags_vec.to(self.device)
+            tags_distrib = F.normalize(tags_vec, dim=1)
             # need to right shift for transformer architecture
             if self.args['arch'] == 'transformer':
                 prev_caption = self.right_shift(caption)
             else:
                 prev_caption = caption
-            out = self.model(img, prev_caption)
+            
+            out, tags_pred = self.model(img, prev_caption)
             # need to flatten the sentence before computing crossentropy loss
             vocab_size = out.size(2)
-            loss = self.criterion(out.reshape(-1, vocab_size), caption.reshape(-1, 1).squeeze())
+            cap_loss = self.criterion(out.reshape(-1, vocab_size), caption.reshape(-1, 1).squeeze())
+            tag_loss = self.criterion(tags_pred, tags_distrib)
+            loss = cap_loss + self.tl_scale * tag_loss
             train_loss += loss
             # backward, might want to control the update step size
             self.optimizer.zero_grad()
@@ -59,14 +65,18 @@ class Trainer():
         self.model.eval()
         with torch.no_grad():
             for i, (img, caption, tags_vec) in enumerate(self.valid_dataloader):
-                img, caption = img.to(self.device), caption.to(self.device)
+                img, caption, tags_vec = img.to(self.device), caption.to(self.device), tags_vec.to(self.device)
+                tags_distrib = F.normalize(tags_vec, dim=1)
                 if self.args['arch'] == 'transformer':
                     prev_caption = self.right_shift(caption)
                 else:
                     prev_caption = caption
-                out = self.model(img, prev_caption)
+                
+                out, tags_pred = self.model(img, prev_caption)
                 vocab_size = out.size(2)
-                loss = self.criterion(out.reshape(-1, vocab_size), caption.reshape(-1, 1).squeeze())
+                cap_loss = self.criterion(out.reshape(-1, vocab_size), caption.reshape(-1, 1).squeeze())
+                tag_loss = self.criterion(tags_pred, tags_distrib)
+                loss = cap_loss + self.tl_scale * tag_loss
                 val_loss += loss
                 val_steps += 1
 
